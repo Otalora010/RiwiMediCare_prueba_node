43c99034bb2819af05b66180adca6a694a935175
santiago otalora <otalorasantiago00@gmail.com>
2026-08-31 07:32:08 -0500

    feat: add medicamento model with almacen relation

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,6 +1,7 @@
 import { Almacen } from './Almacen';
 import { Category } from './Category';
 import { Clinica } from './Clinica';
+import { Medicamento } from './Medicamento';
 import { Resource } from './Resource';
 import { User } from './User';
 
@@ -10,4 +11,7 @@ Resource.belongsTo(User, { foreignKey: 'ownerId', as: 'owner' });
 Category.hasMany(Resource, { foreignKey: 'categoryId', as: 'resources' });
 Resource.belongsTo(Category, { foreignKey: 'categoryId', as: 'category' });
 
-export { Almacen, Category, Clinica, Resource, User };
+Almacen.hasMany(Medicamento, { foreignKey: 'almacenId', as: 'medicamentos' });
+Medicamento.belongsTo(Almacen, { foreignKey: 'almacenId', as: 'almacen' });
+
+export { Almacen, Category, Clinica, Medicamento, Resource, User };
